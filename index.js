0357b97e981ce7bfe562eeb4aa84799b50b28c72
Kiran Johns <kiranjohns369@gmail.com>
2020-10-10 01:28:11 +0530

    fetch static page

--- index.js
+++ index.js
@@ -1,35 +1,36 @@
-async function renderLinks(request){
-	return new Response("Home", { status: 200})
-}
+var links = [
+	{"name": "Link 1", "url": "https://google.com"},
+	{"name": "Link 2", "url": "https://twitter.com"},
+	{"name": "Link 3", "url": "https://cloudflare.com"}
+]
 
 async function handleLinkRequest(request) {
-	const links = [
-		{"name": "Link 1", "url": "https://google.com"},
-		{"name": "Link 2", "url": "https://twitter.com"},
-		{"name": "Link 3", "url": "https://cloudflare.com"}
-	]
-
 	const jsonLinks = JSON.stringify(links,null,2)
 	return new Response(jsonLinks, {
 		headers: { 'content-type': 'application/json;charset=UTF-8' },
 	})
 }
 
-async function pageNotFoundError(request) {
-	return new Response("Page Not Found!",{ status: 404})
+async function getHTML(request){
+	const staticURL = 'https://static-links-page.signalnerve.workers.dev'
+	//const baseURL = new URL(request.url)
+	html = await fetch(staticURL).then((res)=>{
+		return res.text()
+	})
+	console.log(links)
+	return new Response(html, { 
+		headers: { 'content-type': 'text/html' }
+	})
 }
+
 
 
 addEventListener('fetch', event => {
   	const url = new URL(event.request.url)
-  	console.log(url.pathname)
-  	if(url.pathname === '/') {
-  		event.respondWith(renderLinks(event.request))
-  	}
-  	else if(url.pathname === '/links') {
+  	if(url.pathname === '/links') {
 		event.respondWith(handleLinkRequest(event.request))
 	}
 	else {
-		event.respondWith(pageNotFoundError(event.request))
+		event.respondWith(getHTML(event.request))
 	}
 })
